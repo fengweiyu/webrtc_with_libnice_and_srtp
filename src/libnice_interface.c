2759360901f84e49229b03211c252e782b3434b4
fengweiyu <984441750@qq.com>
2020-01-14 20:40:30 +0800

    [CHG]src:libnice使用接口完善

--- src/libnice_interface.c
+++ src/libnice_interface.c
@@ -123,58 +123,118 @@ int LibniceGetLocalCandidate(T_LocalCandidate * i_ptLocalCandidate)
 int LibniceSetRemoteCandidate(char * i_strStunAddr,unsigned int i_dwStunPort)
 {
 
+nice_agent_set_remote_candidates
 
 
 }
+static int
+parse_remote_data(NiceAgent *agent, guint _stream_id,
+    guint component_id, char *line)
+{
+  GSList *remote_candidates = NULL;
+  gchar **line_argv = NULL;
+  const gchar *ufrag = NULL;
+  const gchar *passwd = NULL;
+  int result = EXIT_FAILURE;
+  int i;
 
+  line_argv = g_strsplit_set (line, " \t\n", 0);
+  for (i = 0; line_argv && line_argv[i]; i++) {
+    if (strlen (line_argv[i]) == 0)
+      continue;
+
+    // first two args are remote ufrag and password
+    if (!ufrag) {
+      ufrag = line_argv[i];
+    } else if (!passwd) {
+      passwd = line_argv[i];
+    } else {
+      // Remaining args are serialized canidates (at least one is required)
+      NiceCandidate *c = parse_candidate(line_argv[i], _stream_id);
+
+      if (c == NULL) {
+        g_message("failed to parse candidate: %s", line_argv[i]);
+        goto end;
+      }
+      remote_candidates = g_slist_prepend(remote_candidates, c);
+    }
+  }
+  if (ufrag == NULL || passwd == NULL || remote_candidates == NULL) {
+    g_message("line must have at least ufrag, password, and one candidate");
+    goto end;
+  }
+
+  if (!nice_agent_set_remote_credentials(agent, _stream_id, ufrag, passwd)) {
+    g_message("failed to set remote credentials");
+    goto end;
+  }
+
+  // Note: this will trigger the start of negotiation.
+  if (nice_agent_set_remote_candidates(agent, _stream_id, component_id,
+      remote_candidates) < 1) {
+    g_message("failed to set remote candidates");
+    goto end;
+  }
+
+  result = EXIT_SUCCESS;
+
+ end:
+  if (line_argv != NULL)
+    g_strfreev(line_argv);
+  if (remote_candidates != NULL)
+    g_slist_free_full(remote_candidates, (GDestroyNotify)&nice_candidate_free);
+
+  return result;
+}
 
 static void LibniceCandidateGatheringDone(NiceAgent *i_ptAgent, guint i_dwStreamID,gpointer pData)
 {
-  g_debug("SIGNAL candidate gathering done\n");
+	g_debug("SIGNAL candidate gathering done\n");
 
-  // Candidate gathering is done. Send our local candidates on stdout
-  int iRet = -1;
-  gchar *strLocalUfrag = NULL;
-  gchar *strLocalPassword = NULL;
-  gchar ipaddr[INET6_ADDRSTRLEN];
-  GSList *cands = NULL, *item = NULL;
-  static const gchar *candidate_type_name[] = {"host", "srflx", "prflx", "relay"};
-  
-  if (!nice_agent_get_local_credentials(i_ptAgent, i_dwStreamID,&strLocalUfrag, &strLocalPassword))
-      goto end;
-  cands = nice_agent_get_local_candidates(i_ptAgent, i_dwStreamID, 1);
-  if (cands == NULL)
-      goto end;
-  
-  printf("%s %s", strLocalUfrag, strLocalPassword);
-  
-  for (item = cands; item; item = item->next) 
-  {
-      NiceCandidate *c = (NiceCandidate *)item->data;
-  
-      nice_address_to_string(&c->addr, ipaddr);
-  
-      // (foundation),(prio),(addr),(port),(type)
-      //"candidate:3442447574 1 udp 2122260223 192.168.0.170 54653 typ host generation 0 ufrag gX6M network-id 1"
-      snprintf(g_tLocalCandidate.strCandidateData,sizeof(g_tLocalCandidate.strCandidateData),
-      " %s,%u,%s,%u,%s",
-      c->foundation,
-      c->priority,
-      ipaddr,
-      nice_address_get_port(&c->addr),
-      candidate_type_name[c->type]);
-  }
-  g_tLocalCandidate.iGatheringDoneFlag = 1;
-  iRet = 0;
-  
-  end:
-      if (strLocalUfrag)
-          g_free(strLocalUfrag);
-      if (strLocalPassword)
-          g_free(strLocalPassword);
-      if (cands)
-          g_slist_free_full(cands, (GDestroyNotify)&nice_candidate_free);
-  return iRet;
+	// Candidate gathering is done. Send our local candidates on stdout
+	int iRet = -1;
+	gchar *strLocalUfrag = NULL;
+	gchar *strLocalPassword = NULL;
+	gchar ipaddr[INET6_ADDRSTRLEN];
+	GSList *cands = NULL, *item = NULL;
+	static const gchar *candidate_type_name[] = {"host", "srflx", "prflx", "relay"};
+	static const gchar *transport_name[] = {"udp", "tcp_active", "tcp_passive", "tcp_so"};
+	
+	if (!nice_agent_get_local_credentials(i_ptAgent, i_dwStreamID,&strLocalUfrag, &strLocalPassword))
+		goto end;
+	cands = nice_agent_get_local_candidates(i_ptAgent, i_dwStreamID, 1);
+	if (cands == NULL)
+		goto end;
+
+	for (item = cands; item; item = item->next) 
+	{
+		NiceCandidate *c = (NiceCandidate *)item->data;
+
+		nice_address_to_string(&c->addr, ipaddr);
+
+		// RFC 5245
+		// a=candidate:<foundation> <component-id> <transport> <priority>
+		// <connection-address> <port> typ <candidate-types>
+		// [raddr <connection-address>] [rport <port>]
+		// *(SP extension-att-name SP extension-att-value)
+		//"candidate:3442447574 1 udp 2122260223 192.168.0.170 54653 typ host generation 0 ufrag gX6M network-id 1"
+		snprintf(g_tLocalCandidate.strCandidateData,sizeof(g_tLocalCandidate.strCandidateData),
+		"candidate:%s %u %s %u %s %u typ %s",
+		c->foundation,c->component_id,transport_name[c->transport],c->priority,
+		ipaddr,nice_address_get_port(&c->addr),candidate_type_name[c->type]);
+	}
+	printf("%s ,%s %s", g_tLocalCandidate.strCandidateData,strLocalUfrag, strLocalPassword);
+	g_tLocalCandidate.iGatheringDoneFlag = 1;
+	iRet = 0;
+
+	end:
+		if (strLocalUfrag)
+			g_free(strLocalUfrag);
+		if (strLocalPassword)
+			g_free(strLocalPassword);
+		if (cands)
+			g_slist_free_full(cands, (GDestroyNotify)&nice_candidate_free);
+	return iRet;
 }
 
  static void LibniceNewSelectPair(NiceAgent *agent, guint _stream_id,guint component_id, gchar *lfoundation,gchar *rfoundation, gpointer data)
@@ -188,8 +248,8 @@ static void LibniceComponentStateChanged(NiceAgent *agent, guint _stream_id,guin
     
 	g_debug("SIGNAL: state changed %d %d %s[%d]\n",_stream_id, component_id, state_name[state], state);
 
-	if (state == NICE_COMPONENT_STATE_CONNECTED) 
-	{
+	if (state == NICE_COMPONENT_STATE_READY) 
+	{//协商成功
 	
 	}
 }
