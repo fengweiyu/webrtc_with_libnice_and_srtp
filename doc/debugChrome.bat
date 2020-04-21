eed863712b45488aedead69ae5b145b3a89936ae
fengweiyu <984441750@qq.com>
2020-04-21 19:05:33 +0800

    [ADD]上传调试谷歌浏览器脚本

---
+++ doc/debugChrome.bat
@@ -0,0 +1,8 @@
+set pa=%cd%
+C:
+cd C:\Users\ywf\AppData\Local\Google\Chrome\Application
+chrome.exe --enable-logging --v=1
+cd ../User Data
+echo f|xcopy chrome_debug.log %pa%\chrome_debug.log /Y/K 
+
+pause 
