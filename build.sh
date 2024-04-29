3b2158a621ffa50415cda6a9ce7f33fae860735a
fengweiyu <984441750@qq.com>
2024-04-29 15:14:57 +0800

    [CHG]增加client编译

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ if [ $# == 0 ]; then
     PrintUsage
     exit -1
 else
-    IsServer=$2
+    IsClient=$2
     OutputPath="./build"
 #build目录是给main执行程序编译用的，同时也是直接提供库，类似make install
 #所以内部编译的原则是main的编译脚本(库的使用者)才会依赖build里面的东西，其他模块的编译脚本是不依赖的    
@@ -57,14 +57,16 @@ else
     cd ..
     
     cd example
-    if [ "$IsServer" == "Server" ]; then
-        cp buildServer.sh build.sh -f
-        cp CMakeListsServer.txt CMakeLists.txt -f
+    if [ "$IsClient" == "client" ]; then
+        cp buildClient.sh build.sh -f
+        cp CMakeListsClient.txt CMakeLists.txt -f
         sh build.sh $1
         if [ $? -ne 0 ]; then
             exit -1
         fi
     else
+        cp buildServer.sh build.sh -f
+        cp CMakeListsServer.txt CMakeLists.txt -f
         sh build.sh $1
         if [ $? -ne 0 ]; then
             exit -1
